3eeded962f0554e4c9643590d9b2a7c27abc7722
Oliver Brito <ob.alarcon2001@gmail.com>
2025-06-27 20:03:17 -0400

    update template

--- benchmarking/template.py
+++ benchmarking/template.py
@@ -1,23 +1,35 @@
 from benchmarking import setup_experiment, run_with_timing
 from input_handler import InputHandler
 
-# Override default config
-# @exp.config
-# def default_config():
-#     cases = [
-#         {
-#             "option": "plotly_resampler",
-#             "input_type": "default",
-#         },
-#         {
-#             "option": "sdsl4py",
-#             "input_type": "sdsl4py",
-#         }
-#     ]
-#     n_range = list(range(10000, 350000, 10000))
-
 exp_name = "TEMPLATE"
 exp = setup_experiment(exp_name)
+
+
+@exp.config
+def default_config():
+    cases = [
+        {
+            "option": "Title 01",
+            "input_type": "default"
+        },
+        {
+            "option": "Title 02",
+            "input_type": "compressed_vector",
+            "compress_option": "enc_vector_elias_gamma"
+        },
+        {
+            "option": "Title 03",
+            "input_type": "compressed_vector_downsampler",
+            "downsampler": "LTTBDownsampler",
+            "compress_option": "enc_vector_elias_gamma"
+        },
+        {
+            "option": "Title 04",
+            "input_type": "tsdownsample",
+            "downsampler": "LTTBDownsampler"
+        }
+    ]
+    n_range = list(range(10000, 350000, 10000))
 
 @exp.automain
 def run(cases, iterations, n_range, file_input_list, decimal_places, width, decompressed):
